a7dfcce40ae48f101a19367d811d7cf27ddc5837
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2022-07-12 20:08:36 -0400

    test(worldwind-react): local package development

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
+import { Globe } from 'worldwind-react';
+
 import { Navbar } from '@components/navbar';
-import { WorldMap } from '@components/world-map';
 
 export default function App() {
   return (
     <div className="app">
       <Navbar />
       <header className="app-header">Map</header>
-      <WorldMap />
+      <Globe />
     </div>
   );
 }
