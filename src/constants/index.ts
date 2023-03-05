126d024c38b06711cf8b5b8045e98249c02cb70a
Kyle J. Burda <kylejbdev@gmail.com>
2023-03-05 00:04:20 -0500

    fix: `BASE_API_URL` value to support deployments (#24)
    * feat: dynamically set BASE_API_URL
    
    * chore: set preview port to dev server port
    
    * fix: glob for jest moduleNameMapper

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,8 +3,10 @@ export const NODE_ENV = {
   PRODUCTION: 'production',
 };
 
+const BASE_API_URL = import.meta.env.DEV ? 'http://127.0.0.1:8888' : 'https://clairbuoyant.live';
+
 export const API_VERSION = 'v1';
-export const BASE_API_URI = `http://localhost:8888/api/${API_VERSION}`;
+export const BASE_API_URI = `${BASE_API_URL}/api/${API_VERSION}`;
 export const API_ROUTES = {
   BUOYS: `${BASE_API_URI}/buoys`,
   COASTLINES: `${BASE_API_URI}/coastlines`,
